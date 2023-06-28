6941cdc1eacda967ffe7a178d831949242f4fcfe
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-06-28 10:59:22 +0000

    conectado con API

--- src/js/component/form.jsx
+++ src/js/component/form.jsx
@@ -26,7 +26,7 @@ const Form = () => {
     const handleKeyPress = (e) => {
         if (e.key === "Enter") {
             e.preventDefault();
-            pushTarea();
+            pushTarea();            
         }
     };
 
@@ -48,11 +48,82 @@ const Form = () => {
         setTareas(newArr)
     }
 
+    //Crear usuario en API
+    function createdUser() {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/bertablancpastor', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify([]), // body data type must match "Content-Type" header
+        })//busca informacion a la url dada con el metodo especificado
+            .then((response) => response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+            .then((data) => console.log(data))// => guardo el json en un espacio de memoria
+            .catch((error) => console.log(error))// => te aviso si algo sale mal
+    }
+
+    //Update the entire list of todo's of a particular user
+    function actualizarTarea() {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/bertablancpastor', {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(tareas), // body data type must match "Content-Type" header
+        })//busca informacion a la url dada con el metodo especificado
+            .then((response) => response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+            .then((data) => console.log(data))// => guardo el json en un espacio de memoria
+            .catch((error) => console.log(error))// => te aviso si algo sale mal
+    }
+
+    //List de tareas
+    function traerListTarea() {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/bertablancpastor', {
+            method: 'GET',
+        })//busca informacion a la url dada con el metodo especificado
+            .then((response) => { 
+                if (response.status === 404) {
+                    createdUser()                 
+                }; // el código de estado = 200 o código = 400 etc. 
+                return response.json()})// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+            .then((data) => console.log(data))// => guardo el json en un espacio de memoria
+            .catch((error) => console.log(error))// => te aviso si algo sale mal
+    }
+
+    useEffect(()=>{
+        traerListTarea()
+        
+    },[])
+
+    useEffect(()=>{//2. Cuando quiero ejecutar una funcion si cambia un estado, es decir utlizamos el array como un observador
+        //bloque de codigo que quiero que se ejecute cuando cambia lo que estoy vigilando
+        if (tareas !=0) {
+            actualizarTarea()
+        }
+    },[tareas])
+
+    
     return (
-        <div className="container mt-5">
-            <div className="">
+        <div className="container mt-5 ">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary mb-5 text-center">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">TO DO</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                {/* <a className="nav-link active" aria-current="page" href="#"  onClick={createdUser}>Crear usuario</a> */}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {/* <div className="d-flex  text-center">
                 <p className="fs-2 text-center display-2">TO DO</p>
-            </div>
+                <button className="btn btn-success" onClick={createdData}>Crear usuario</button>
+            </div> */}
             <form>
                 <div className="mb-3">
                     <input type="text" onChange={handleTarea} onKeyPress={handleKeyPress} className="form-control" id="form2" placeholder="Write a todo" value={tarea} />
@@ -60,13 +131,13 @@ const Form = () => {
             </form>
             <button type="button" onClick={pushTarea} className="btn btn-info ms-2t">Add</button>
 
-            <ul className="list-group mt-4">{tareas.map((item, index) => 
-                <li key={index} className={`d-flex justify-content-between align-items-center list-group-item ${item.done ? "text-decoration-line-through" : ""}`}>{item.label} 
+            <ul className="list-group mt-4">{tareas.map((item, index) =>
+                <li key={index} className={`d-flex justify-content-between align-items-center list-group-item ${item.done ? "text-decoration-line-through" : ""}`}>{item.label}
                     <span onClick={() => completarTarea(item.label)}>
                         <i className="fa-solid fa-square-check"></i>
                     </span>
                     <span className="delete-icon" onClick={() => borrarTarea(item.label)}
-                        ><i className="fa-solid fa-trash"></i>
+                    ><i className="fa-solid fa-trash"></i>
                     </span>
                 </li>)}
             </ul>
